0ea243888ca868c428ba75c87a47f7808fc42a5d
Felipe Almeida <felipe.paulino@fcamara.com.br>
2019-04-06 14:23:42 -0300

    Initial Commit

---
+++ timefollower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timefollower
+//
+//  Created by Felipe Almeida on 06/04/19.
+//  Copyright © 2019 JAÉ Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
